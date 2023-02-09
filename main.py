f9b48e9cc0f96750bd21d24e1829e2a098aa4f13
Simon <fiewind1@gmail.com>
2023-02-09 14:57:47 +0200

    used append and Bracket functions to see the char and the position

--- main.py
+++ main.py
@@ -14,11 +14,12 @@ def find_mismatch(text):
     for i, next in enumerate(text):
         if next in "([{":
             # Process opening bracket, write your code here
-            opening_brackets_stack = opening_brackets_stack
+            opening_brackets_stack.append(Bracket(next,i))
 
         if next in ")]}":
             # Process closing bracket, write your code here
             pass
+    print(opening_brackets_stack)
 
 
 def main():
